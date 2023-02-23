107d573092169f63a76d649a088d3a9731990d91
Gavin Coleman <gavinjcoleman@gmail.com>
2023-02-23 09:57:15 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 

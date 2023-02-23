e41b597ebbd84f9fe34b0fbdf460c46d0cf87a37
Gavin Coleman <gavinjcoleman@gmail.com>
2023-02-23 14:49:34 +0000

    Working fine

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc 		Register a new user
 // @route 	/api/users
 //@access 	Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
 
 	// Validation
@@ -10,14 +12,14 @@ const registerUser = (req, res) => {
 		throw new Error('Please include all fields');
 	}
 	res.send('Register Route');
-};
+});
 
 // @desc 		Login a user
 // @route 	/api/users/login
 //@access 	Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
 	res.send('Login Route');
-};
+});
 
 module.exports = {
 	registerUser,

0da925d495ca5c3d31dccdc7b0ac9e270e2c6b45
Gavin Coleman <gavinjcoleman@gmail.com>
2023-03-29 10:08:44 +0100

    Working fine to 4:20 on video

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -20,6 +20,35 @@ const getTickets = asyncHandler(async (req, res) => {
 	res.status(200).json(tickets);
 });
 
+// @desc 		Get user ticket
+// @route 	GET/api/tickets/:id
+//@access 	Private
+const getTicket = asyncHandler(async (req, res) => {
+	// Get user using the id in the JSON Web Token (JWT)
+	const user = await User.findById(req.user.id);
+
+	if (!user) {
+		res.status(401);
+		throw new Error('User not found');
+	}
+
+	const ticket = await Ticket.findById(req.params.id);
+
+	// Throw error if ticket not found
+	if (!ticket) {
+		res.status(404);
+		throw new Error('Ticket not found');
+	}
+
+	// Throw error if the user does not match the ticket user
+	if (ticket.user.toString() !== req.user.id) {
+		res.status(401);
+		throw new Error('Not Authorized');
+	}
+
+	res.status(200).json(ticket);
+});
+
 // @desc 		Create new ticket
 // @route 	POST/api/tickets
 //@access 	Private
@@ -51,5 +80,6 @@ const createTicket = asyncHandler(async (req, res) => {
 
 module.exports = {
 	getTickets,
+	getTicket,
 	createTicket,
 };

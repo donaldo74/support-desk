87d844a010c3dec582214025a31882e9f147e7eb
Gavin Coleman <gavinjcoleman@gmail.com>
2023-04-06 19:44:40 +0100

    ok to 7:12 on video

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTicket, reset } from '../features/tickets/ticketSlice'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
 
 function Ticket() {
+	const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+		(state) => state.tickets
+	)
+
+	const params = useParams()
+
+	useEffect(() => {
+		if (isError) {
+		}
+	})
+
 	return <div>Ticket</div>
 }
 

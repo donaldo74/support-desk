d47a6efba44a95036a1ffbc536ed7aab40a9e7d4
Gavin Coleman <gavinjcoleman@gmail.com>
2023-04-18 18:40:34 +0100

    All working fine

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -16,7 +16,48 @@ const getNotes = asyncHandler(async (req, res) => {
 		throw new Error('User not found')
 	}
 
-	const tickets = await Ticket.find({ user: req.user.id })
+	const ticket = await Ticket.findById(req.params.ticketId)
 
-	res.status(200).json(tickets)
+	if (ticket.user.toString() !== req.user.id) {
+		res.status(401)
+		throw new Error('User not authorized')
+	}
+
+	const notes = await Note.find({ ticket: req.params.ticketId })
+
+	res.status(200).json(notes)
 })
+
+// @desc 		Create ticket note
+// @route 	POST/api/tickets/:ticketId/notes
+//@access 	Private
+const addNote = asyncHandler(async (req, res) => {
+	// Get user using the id in the JSON Web Token (JWT)
+	const user = await User.findById(req.user.id)
+
+	if (!user) {
+		res.status(401)
+		throw new Error('User not found')
+	}
+
+	const ticket = await Ticket.findById(req.params.ticketId)
+
+	if (ticket.user.toString() !== req.user.id) {
+		res.status(401)
+		throw new Error('User not authorized')
+	}
+
+	const note = await Note.create({
+		text: req.body.text,
+		isStaff: false,
+		ticket: req.params.ticketId,
+		user: req.user.id,
+	})
+
+	res.status(200).json(note)
+})
+
+module.exports = {
+	getNotes,
+	addNote,
+}

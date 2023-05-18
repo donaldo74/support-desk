40fa3b93d5da6a97fb3cb2b68bf9638837baa9c1
Gavin Coleman <gavinjcoleman@gmail.com>
2023-05-18 19:10:40 +0100

    changed req to _

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,11 @@ if (process.env.NODE_ENV === 'production') {
 	// Set build folder as static
 	app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-	app.get('*', (req, res) =>
+	app.get('*', (_, res) =>
 		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
 	)
 } else {
-	app.get('/', (req, res) => {
+	app.get('/', (_, res) => {
 		res.status(200).json({ message: 'Welcome to the Support Desk API' })
 	})
 }
